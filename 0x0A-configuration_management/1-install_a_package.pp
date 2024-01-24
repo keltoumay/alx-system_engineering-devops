556eea987312feee469aee08bd9983a5c56353c4
keltoumy <keltoumaouyahia@gmail.com>
2024-01-24 15:56:33 -0800

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-# Installs flask, version 2.1.0.
-
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+puppet
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
